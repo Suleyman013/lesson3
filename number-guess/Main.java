59cfbc21771fb7d9081483fa0b008bbf655e1d66
Suleyman013 <suleyman.mammadov.std@bhos.edu.az>
2025-02-07 17:21:46 +0400

    Add files via upload

---
+++ number-guess/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int Randomnum = (int) (Math.random() * 101);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Let the game begin!");
+        System.out.println("Please enter your name:");
+        String name = sc.nextLine();
+        int[] array = {};
+        int attempt = 0;
+        boolean guessed = true;
+        System.out.println("Please enter your number:");
+        while (guessed) {
+
+            int number = sc.nextInt();
+            if (number < Randomnum) {
+                System.out.println("Your number is too small. Please, try again.");
+                array = Arrays.copyOf(array, array.length + 1);
+                array[attempt] = number;
+                attempt++;
+            } else if (number > Randomnum) {
+                System.out.println("Your number is too big. Please, try again.");
+                array = Arrays.copyOf(array, array.length + 1);
+                array[attempt] = number;
+                attempt++;
+            } else if (number == Randomnum) {
+                System.out.println("Congratulations " + name + "!");
+                guessed = false;
+            }
+
+            System.out.println("Please enter your number:");
+
+
+        }
+        Arrays.sort(array);
+        System.out.println("Your numbers are:");
+        for (int i = 0; i < attempt; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
